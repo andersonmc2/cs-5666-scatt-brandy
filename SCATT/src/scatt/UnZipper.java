7dae94812033c3637f00635e184d937875dd0596
Matt Stone <mattstone22133@gmail.com>
2017-03-20 10:16:49 -0400

    Minimal example of how to unzip a file. Warning: this isn't complete yet and will fill up the src directory! I will make this have its own output directory when I get more time.

---
+++ SCATT/src/scatt/UnZipper.java
@@ -0,0 +1,66 @@
+package scatt;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * @author Matt Stone
+ * @version 1.0
+ * 
+ */
+public class UnZipper
+{
+    final static int BUFFER = 2048;
+
+    /**
+     * @param argv cmd line args
+     */
+    public static void main(String argv[])
+    {
+        try
+        {
+            String buildPath = System.getProperty("user.dir");
+            String path = "Pong Starter.sb2";
+            path = buildPath + "\\src\\scatt\\" + path;
+            String outputPath = "";
+            //String outputPath = "\\src\\scatt\\output\\";
+            File ouputDir = new File(buildPath + outputPath);
+            ouputDir.mkdir();
+
+            // feed bos to file
+            BufferedOutputStream dest = null;
+
+            // for reading the zip file
+            FileInputStream fis = new FileInputStream(path);
+            ZipInputStream zis = new ZipInputStream(
+                    new BufferedInputStream(fis));
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null)
+            {
+                System.out.println("Extracting: " + entry);
+                int count;
+                byte data[] = new byte[BUFFER];
+                // write the files to the disk
+                FileOutputStream fos = new FileOutputStream(outputPath
+                        + entry.getName());
+                dest = new BufferedOutputStream(fos, BUFFER);
+                while ((count = zis.read(data, 0, BUFFER)) != -1)
+                {
+                    dest.write(data, 0, count);
+                }
+                dest.flush();
+                dest.close();
+            }
+            zis.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
